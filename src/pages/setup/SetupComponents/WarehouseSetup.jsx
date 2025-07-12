ae3316f7be218551133ba83d6e6d2b14ae6315f0
AMyasir65 <abdullah.yasir@jkcsoftwares.com>
2025-07-12 10:13:27 +0530

    feat: implement WarehouseSetup component for warehouse configuration

---
+++ src/pages/setup/SetupComponents/WarehouseSetup.jsx
@@ -0,0 +1,71 @@
+// SetupComponents/WarehouseSetup.jsx
+import React, { memo } from "react";
+import InputBoxStream from "../../../components/FormComponent/InputBoxStream";
+import DropdownStream from "../../../components/FormComponent/DropdownStream";
+import SetupTemplate from "../../../components/core/SetupTemplate";
+import { getOption } from "../../../utils/FormHelper";
+import styles from "./WarehouseSetup.module.css";
+
+const WarehouseSetup = memo(
+  ({ setPage, locationFormData, setLocationFormData, finish, LocationMetadata }) => {
+    const handleLocationChange =
+      (type, name, value) =>
+      (e) => {
+        if (type === "autocomplete") {
+          setLocationFormData((prevState) => ({
+            ...prevState,
+            [name]: value,
+          }));
+        } else {
+          setLocationFormData((prevState) => ({
+            ...prevState,
+            [name]: e.target.value,
+          }));
+        }
+      };
+
+    const main = (
+      <div className={styles.mainContainer}>
+        {LocationMetadata.map((location) => {
+          if (location.toAdd) {
+            return (
+              <div key={location.name} className={styles.formItem}>
+                {location.autocomplete ? (
+                  <DropdownStream
+                    moreVisible={true}
+                    field={location}
+                    options={getOption("state")}
+                    required={true}
+                    handleChange={handleLocationChange}
+                  />
+                ) : (
+                  <InputBoxStream
+                    moreVisible={true}
+                    field={location}
+                    value={locationFormData[location.name]}
+                    handleChange={handleLocationChange}
+                  />
+                )}
+              </div>
+            );
+          }
+          return null; 
+        })}
+      </div>
+    );
+
+    return (
+      <SetupTemplate
+        heading="Add a [Warehouse]."
+        main={main}
+        navigation="final"
+        setPage={setPage}
+        finish={finish}
+      />
+    );
+  },
+);
+
+WarehouseSetup.displayName = "WarehouseSetup";
+
+export default WarehouseSetup;
