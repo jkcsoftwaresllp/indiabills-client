44e19ceb23e1b4a32abc485fb44fb952b578abf0
AMyasir65 <abdullah.yasir@jkcsoftwares.com>
2026-01-19 18:44:45 +0530

    added route for domain-specific registration

--- src/routes/authRoutes.jsx
+++ src/routes/authRoutes.jsx
@@ -9,6 +9,7 @@ const AuthRoutes = () => {
   return [
   <Route key="login" path="/login" element={<LoginPage />} />,
   <Route key="register" path="/register" element={<Register />} />,
+  <Route key="register-domain" path="/register/:domain" element={<PublicRegister />} />,
   <Route key="organization-selector" path="/organization-selector" element={<OrganizationSelector />} />,
   <Route key="organization-create" path="/organization/create" element={<CreateOrganization />} />
 ];
